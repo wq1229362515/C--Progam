979d6ebd45048af330a78123947965e20cde054c
wq1229362515 <1229362515@qq.com>
2019-06-03 23:09:02 +0800

    第一次上传部分没有上传上去

---
+++ 6.3C++前端IU.net/6.3C++/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _6._3C__
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "wq")
+            {
+            Form2 fom = new Form2();
+            fom.Show();
+            this.Hide();
+            }
+            else
+            {
+                return;
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ 6.3C++前端IU.net/6.3C++/Form2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _6._3C__
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            DataRow row = dataTable1.NewRow();
+            row[0]= textBox1.Text.ToString();
+            row[1]= textBox2.Text.ToString();
+            row[2]= textBox3.Text.ToString();
+            row[3]= textBox4.Text.ToString();
+            dataTable1.Rows.Add(row);
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = dataTable1.Rows[0]["姓名"].ToString();
+            textBox2.Text = dataTable1.Rows[0]["学号"].ToString();
+            textBox3.Text = dataTable1.Rows[0]["性别"].ToString();
+            textBox4.Text = dataTable1.Rows[0]["年龄"].ToString();
+            dataTable1.Rows.Clear();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form1 fom = new Form1();
+            fom.Show();
+            this.Hide();
+        }
+    }
+}
