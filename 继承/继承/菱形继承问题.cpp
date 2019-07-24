62f53cf660e4228d4a8455570cafbd2bb27fcd7d
wq1229362515 <1229362515@qq.com>
2019-07-24 22:23:19 +0800

    7.24 菱形继承

--- 继承/继承/菱形继承问题.cpp
+++ 继承/继承/菱形继承问题.cpp
@@ -44,7 +44,7 @@ void Test(){
 
 	//这样就可以解决代码的二义性,但是数据的冗余性并没有得到解决
 	//关于这种问题虚拟继承既可以解决问题
-	//使用virtual既可以解决这种问题,数据就都会统一了5
+	//使用virtual既可以解决这种问题,数据也会统一了
 	test.Student::_name = "Li";
 	test.Teacher::_name = "Wang";
 }
@@ -57,17 +57,21 @@ int main(){
 }
 #endif
 
+/*
+	使用菱形虚拟继承,这样避免了数据的冗余
+
+*/
 class A {
 public:
 	int _a;
 };
 
-class B : virtual public A {
+class B :  virtual public A {
 public:
 	int _b;
 };
 
-class C : virtual public A {
+class C :virtual public A {
 public:
 	int _c;
 };
@@ -77,13 +81,30 @@ public:
 	int _d;
 };
 
-int main() {
+/*
+	使用普通的菱形继承
+0x007EFAF0  01 00 00 00  ....
+0x007EFAF4  03 00 00 00  ....
+0x007EFAF8  02 00 00 00  ....
+0x007EFAFC  04 00 00 00  ....
+0x007EFB00  05 00 00 00  ....
+
+使用菱形继承就可以解决数据冗余的问题,因为虚基表指针的缘故,指向了虚基表,可以定位数据冗余距离自己的位置
+通过这个位置,在修改内容的时候,就可以通过指针来操作,最终找到这个数据冗余的位置,从而修改
+
+通过我们的 类名:成员变量可以解决数据二义性的问题,
+通过virtual 则可以将二义性和数据冗余都给解决掉
+*/
+void Test2(){
 	D d;
-	d.B::_a = 1;
 	d.C::_a = 2;
+	d.B::_a = 1;
 	d._b = 3;
 	d._c = 4;
 	d._d = 5;
+}
+int main() {
 
+	Test2();
 	return 0;
 }
